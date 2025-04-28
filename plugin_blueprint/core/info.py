3593c6883ab05a3c89bab381513b3761fd7fa055
Moritz Schott <moritz.schott@heigit.org>
2025-04-28 17:39:03 +0000

    dep(climatoology): update climatoology

--- plugin_blueprint/core/info.py
+++ plugin_blueprint/core/info.py
@@ -2,15 +2,16 @@ import importlib
 import importlib.metadata
 from pathlib import Path
 
-from climatoology.base.info import _Info, generate_plugin_info
+from climatoology.base.info import PluginAuthor, _Info, generate_plugin_info
 from semver import Version
 
 
 def get_info() -> _Info:
+    authors = [PluginAuthor(name='Blueprint Author')]
     info = generate_plugin_info(
         name='Plugin Blueprint',
         icon=Path('resources/icon.jpeg'),
-        authors=[],
+        authors=authors,
         version=Version.parse(importlib.metadata.version('plugin-blueprint')),
         concerns=set(),
         purpose=Path('resources/purpose.md'),
