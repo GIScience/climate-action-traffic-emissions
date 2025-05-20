45c55f5f2c6acb6ad0be4845e02c7e1e48d6bb66
Danielle Gatland <danielle.gatland@heigit.org>
2025-05-20 14:38:33 +0000

    dep: update base env and config for new climatoology
    ...and new logo

--- plugin_blueprint/core/info.py
+++ plugin_blueprint/core/info.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from climatoology.base.info import PluginAuthor, _Info, generate_plugin_info
 from semver import Version
 
+from plugin_blueprint.core.input import ComputeInput
+
 
 def get_info() -> _Info:
     authors = [PluginAuthor(name='Blueprint Author')]
@@ -15,6 +17,8 @@ def get_info() -> _Info:
         version=Version.parse(importlib.metadata.version('plugin-blueprint')),
         concerns=set(),
         purpose=Path('resources/purpose.md'),
+        teaser='A bare minimum example to act as a blueprint for new assessment tools on the CA Platform.',
         methodology=Path('resources/methodology.md'),
+        demo_input_parameters=ComputeInput(),
     )
     return info
